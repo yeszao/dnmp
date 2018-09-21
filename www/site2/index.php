376f628d7f0f81ce34ab5f73a3222b91904c5086
yeszao <yeszao@qq.com>
2018-09-21 17:49:22 +0800

    set site2/index.php same as site1/index.php

--- www/site2/index.php
+++ www/site2/index.php
@@ -1,12 +1,14 @@
 <?php
 
 echo '<h1 style="text-align: center;">欢迎使用DNMP！</h1>';
-
 echo '<h2>版本信息</h2>';
-echo '<p>PHP版本：<code>', phpversion(), '</code></p>';
-echo '<p>Nginx版本：<code>', $_SERVER['SERVER_SOFTWARE'], '</code></p>';
-echo '<p>MySQL版本：<code>', getMysqlVersion(), '</code></p>';
-echo '<p>Redis版本：<code>', getRedisVersion(), '</code></p>';
+
+echo '<ul>';
+echo '<li>PHP版本：<code>', PHP_VERSION, '</code></li>';
+echo '<li>Nginx版本：<code>', $_SERVER['SERVER_SOFTWARE'], '</code></li>';
+echo '<li>MySQL服务器版本：<code>', getMysqlVersion(), '</code></li>';
+echo '<li>Redis服务器版本：<code>', getRedisVersion(), '</code></li>';
+echo '</ul>';
 
 echo '<h2>已安装扩展</h2>';
 printExtensions();
@@ -52,9 +54,10 @@ function getRedisVersion()
  */
 function printExtensions()
 {
-    $extensions = get_loaded_extensions();
-    foreach ($extensions as $i => $e) {
-        echo $i + 1, '. ', $e, '<br />';
+    echo '<ol>';
+    foreach (get_loaded_extensions() as $i => $name) {
+        echo "<li>", $name, '=<code>', phpversion($name), '</code></li>';
     }
+    echo '</ol>';
 }
 
