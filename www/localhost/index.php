345801720d891fadb7a92fad7310dcd00861588d
yeszao <yeszao@qq.com>
2018-09-26 09:54:03 +0800

    change .gitignore

---
+++ www/localhost/index.php
@@ -0,0 +1,67 @@
+<?php
+
+echo '<h1 style="text-align: center;">欢迎使用DNMP！</h1>';
+echo '<h2>版本信息</h2>';
+
+echo '<ul>';
+echo '<li>PHP版本：', PHP_VERSION, '</li>';
+echo '<li>Nginx版本：', $_SERVER['SERVER_SOFTWARE'], '</li>';
+echo '<li>MySQL服务器版本：', getMysqlVersion(), '</li>';
+echo '<li>Redis服务器版本：', getRedisVersion(), '</li>';
+echo '</ul>';
+
+echo '<h2>已安装扩展</h2>';
+printExtensions();
+
+
+/**
+ * 获取MySQL版本
+ */
+function getMysqlVersion()
+{
+    if (extension_loaded('PDO_MYSQL')) {
+        try {
+            $dbh = new PDO('mysql:host=mysql;dbname=mysql', 'root', '123456');
+            $sth = $dbh->query('SELECT VERSION() as version');
+            $info = $sth->fetch();
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+        return $info['version'];
+    } else {
+        return 'PDO_MYSQL 扩展未安装 ×';
+    }
+
+}
+
+/**
+ * 获取Redis版本
+ */
+function getRedisVersion()
+{
+    if (extension_loaded('redis')) {
+        try {
+            $redis = new Redis();
+            $redis->connect('redis', 6379);
+            $info = $redis->info();
+            return $info['redis_version'];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    } else {
+        return 'Redis 扩展未安装 ×';
+    }
+}
+
+/**
+ * 获取已安装扩展列表
+ */
+function printExtensions()
+{
+    echo '<ol>';
+    foreach (get_loaded_extensions() as $i => $name) {
+        echo "<li>", $name, '=', phpversion($name), '</li>';
+    }
+    echo '</ol>';
+}
+
