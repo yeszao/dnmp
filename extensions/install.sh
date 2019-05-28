1b0429779ed79248fd553b5c10672b139e40d2a7
yeszao <yeszao@qq.com>
2019-05-28 16:21:25 +0800

    move curl-dev package to top

--- extensions/install.sh
+++ extensions/install.sh
@@ -18,7 +18,7 @@ fi
 
 if [ "${PHP_EXTENSIONS}" != "" ]; then
     echo "---------- Install general dependencies ----------"
-    apk add --no-cache autoconf g++ libtool make openssl-dev libxml2-dev linux-headers
+    apk add --no-cache autoconf g++ libtool make curl-dev libxml2-dev linux-headers
 fi
 
 if [ -z "${EXTENSIONS##*,pdo_mysql,*}" ]; then
@@ -196,8 +196,6 @@ fi
 
 if [ -z "${EXTENSIONS##*,curl,*}" ]; then
     echo "---------- Install curl ----------"
-	apk add --no-cache curl
-	apk add --no-cache curl-dev
 	docker-php-ext-install ${MC} curl
 fi
 
