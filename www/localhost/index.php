9f4e2928fe341529b8b9fd59df62935e01dc4ff0
yeszao <yeszao@qq.com>
2019-09-28 08:33:56 +0800

    Add mongodb connect example

--- www/localhost/index.php
+++ www/localhost/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 echo '<h1 style="text-align: center;">欢迎使用DNMP！</h1>';
 echo '<h2>版本信息</h2>';
@@ -8,6 +10,7 @@ echo '<li>PHP版本：', PHP_VERSION, '</li>';
 echo '<li>Nginx版本：', $_SERVER['SERVER_SOFTWARE'], '</li>';
 echo '<li>MySQL服务器版本：', getMysqlVersion(), '</li>';
 echo '<li>Redis服务器版本：', getRedisVersion(), '</li>';
+echo '<li>MongoDB服务器版本：', getMongoVersion(), '</li>';
 echo '</ul>';
 
 echo '<h2>已安装扩展</h2>';
@@ -50,6 +53,27 @@ function getRedisVersion()
         }
     } else {
         return 'Redis 扩展未安装 ×';
+    }
+}
+
+/**
+ * 获取MongoDB版本
+ */
+function getMongoVersion()
+{
+    if (extension_loaded('mongodb')) {
+        try {
+            $manager = new MongoDB\Driver\Manager('mongodb://root:123456@mongodb:27017');
+            $command = new MongoDB\Driver\Command(array('serverStatus'=>true));
+
+            $cursor = $manager->executeCommand('admin', $command);
+
+            return $cursor->toArray()[0]->version;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    } else {
+        return 'MongoDB 扩展未安装 ×';
     }
 }
 
