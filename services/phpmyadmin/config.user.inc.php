785a1bdee790e0eb3d44c0d2c5ea8ae8e7221b99
镜面 <153555712@qq.com>
2019-11-04 15:45:36 +0800

    update config.user.inc example

--- services/phpmyadmin/config.user.inc.php
+++ services/phpmyadmin/config.user.inc.php
@@ -2,3 +2,18 @@
 /**
  * @example https://docs.phpmyadmin.net/en/latest/config.html
  */
+/*mysql,mysql5 example
+$cfg['blowfish_secret']='multiServerExample70518';
+
+$i = 0;
+
+$i++; // server 1 :
+$cfg['Servers'][$i]['verbose']   = 'no1';
+$cfg['Servers'][$i]['host']      = 'mysql';
+
+
+$i++; // server 2 :
+$cfg['Servers'][$i]['verbose']   = 'no2';
+$cfg['Servers'][$i]['host']      = 'mysql5';
+*/
+
