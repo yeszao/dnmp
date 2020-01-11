65bcfc00f2abeacbe59b07cb69ffa26038e3d8a2
yeszao <yeszao@qq.com>
2020-01-11 23:35:20 +0800

    Remove gd installation option

--- services/php/extensions/install.sh
+++ services/php/extensions/install.sh
@@ -207,8 +207,6 @@ if [[ -z "${EXTENSIONS##*,gd,*}" ]]; then
         libjpeg-turbo \
         libjpeg-turbo-dev \
     && docker-php-ext-configure gd \
-        --with-freetype-dir=/usr/include/ \
-        --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install ${MC} gd \
     && apk del \
         freetype-dev \
