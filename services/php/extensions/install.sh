4daa530c79d9d472e805bc1362b4f1853360ad94
yeszao <yeszao@qq.com>
2020-02-04 12:42:40 +0800

    Fix gd unrecognized options error

--- services/php/extensions/install.sh
+++ services/php/extensions/install.sh
@@ -199,6 +199,16 @@ fi
 
 if [[ -z "${EXTENSIONS##*,gd,*}" ]]; then
     echo "---------- Install gd ----------"
+    isPhpVersionGreaterOrEqual 7 4
+
+    if [[ "$?" = "1" ]]; then
+        # "--with-xxx-dir" was removed from php 7.4,
+        # issue: https://github.com/docker-library/php/issues/912
+        options="--with-freetype --with-jpeg"
+    else
+        options="--with-gd --with-freetype-dir=/usr/include/ --with-png-dir=/usr/include/ --with-jpeg-dir=/usr/include/"
+    fi
+
     apk add --no-cache \
         freetype \
         freetype-dev \
@@ -206,11 +216,7 @@ if [[ -z "${EXTENSIONS##*,gd,*}" ]]; then
         libpng-dev \
         libjpeg-turbo \
         libjpeg-turbo-dev \
-    && docker-php-ext-configure gd \
-        --with-gd \
-        --with-freetype-dir=/usr/include/ \
-        --with-png-dir=/usr/include/ \
-        --with-jpeg-dir=/usr/include/ \
+    && docker-php-ext-configure gd ${options} \
     && docker-php-ext-install ${MC} gd \
     && apk del \
         freetype-dev \
