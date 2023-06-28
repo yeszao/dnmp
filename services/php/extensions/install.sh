157cf6d42f8bc5fc6437f24c334de0c312115a89
jingmian <153555712@qq.com>
2023-06-28 16:20:15 +0800

    update php74 swoole-4.8.11

--- services/php/extensions/install.sh
+++ services/php/extensions/install.sh
@@ -621,7 +621,7 @@ if [[ -z "${EXTENSIONS##*,swoole,*}" ]]; then
     isPhpVersionGreaterOrEqual 7 0
 
     if [[ "$?" = "1" ]]; then
-        installExtensionFromTgz swoole-4.5.2 --enable-openssl
+        installExtensionFromTgz swoole-4.8.11 --enable-openssl
     else
         installExtensionFromTgz swoole-2.0.11
     fi
